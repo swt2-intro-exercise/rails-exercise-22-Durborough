38c19293067e390fa7958f0a8549421eb51aa0c6
ole.schmitt <ole.schmitt@student.hpi.de>
2022-11-13 17:29:57 +0100

    Solve #16

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -37,6 +37,6 @@ class AuthorsController < ApplicationController
     end
     private
         def author_params
-            params.require(:author).permit(:first_name, :last_name, :homepage)
+            params.require(:author).permit(:first_name, :last_name, :homepage, {:paper_ids => []})
         end
 end
